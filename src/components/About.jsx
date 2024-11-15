1ca8b3301ae8b9991602d3803964cfe4942de5db
Ganesh <gapawar2003@gmail.com>
2024-11-15 20:53:29 +0530

    change 3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
     {
       icon: Code,
       title: "Competitive Programming",
-      description: "LeetCode rating: 1531 | 350+ problems solved",
+      description: "LeetCode rating: 1628 | 400+ problems solved",
     },
     {
       icon: Award,
@@ -111,7 +111,7 @@ const About = () => {
               } delay-600`}
             >
               <a
-                href="/public/Resume.pdf"
+                href="https://drive.google.com/drive/folders/1qos9TOK_Lm7rERJieQCk8hSfiGtF8OEG?usp=drive_link"
                 className="group flex items-center gap-2 px-6 py-3 bg-purple-600 rounded-full hover:bg-purple-700 transition-all duration-300 hover:scale-105"
               >
                 <FileDown className="w-5 h-5" />
